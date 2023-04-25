f294e61add084a2e34af13a83a7458428a7a34f8
patrick Al Helou <Patrickalhelou@gmail.com>
2023-04-25 11:16:46 +0300

    login and register with database

---
+++ app/src/main/java/com/example/mobileproject/RegisterActivity.java
@@ -0,0 +1,93 @@
+package com.example.mobileproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText edUsername , edPassword , edConfirm , edEmail;
+    Button btn;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        edUsername = findViewById(R.id.editTextRegUsername);
+        edPassword = findViewById(R.id.editTextTextRegPassword);
+        edEmail = findViewById(R.id.editTextRegEmail);
+        edConfirm = findViewById(R.id.editTextRegConfirmPassword);
+        btn = findViewById(R.id.buttonRegister);
+        tv = findViewById(R.id.textViewExistingUser);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edUsername.getText().toString();
+                String email = edEmail.getText().toString();
+                String password = edPassword.getText().toString();
+                String confirm = edConfirm.getText().toString();
+                Database db = new Database(getApplicationContext() , "fitness",null,1);
+
+                if(username.length() == 0  || password.length() == 0 || password.length() == 0 || confirm.length()==0){
+
+                    Toast.makeText(RegisterActivity.this, "Please fill all the details", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if (password.compareTo(confirm) == 0) {
+                        if (isValid(password)) {
+                            db.register(username,email,password);
+                            Toast.makeText(RegisterActivity.this, "Record inserted", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Passwords does not respect validation", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Passwords does not match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+
+    public static boolean isValid(String password){
+        // Password must be at least 8 characters long
+        if (password.length() < 8) {
+            return false;
+        }
+        // Password must contain at least one uppercase letter
+        if (!password.matches(".*[A-Z].*")) {
+            return false;
+        }
+        // Password must contain at least one lowercase letter
+        if (!password.matches(".*[a-z].*")) {
+            return false;
+        }
+        // Password must contain at least one digit
+        if (!password.matches(".*\\d.*")) {
+            return false;
+        }
+        // Password must contain at least one special character
+        if (!password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*")) {
+            return false;
+        }
+        // Password is valid
+        return true;
+    }
+}
