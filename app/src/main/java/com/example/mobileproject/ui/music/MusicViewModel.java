8bafdb1e8f82caf655dfd91b248d4f129dda596c
patrick Al Helou <Patrickalhelou@gmail.com>
2023-05-06 13:10:39 +0300

    id increment and start with sleep

--- app/src/main/java/com/example/mobileproject/ui/music/MusicViewModel.java
+++ app/src/main/java/com/example/mobileproject/ui/music/MusicViewModel.java
@@ -1,14 +1,14 @@
-package com.example.mobileproject.ui.sleep;
+package com.example.mobileproject.ui.music;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SleepViewModel extends ViewModel {
+public class MusicViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public SleepViewModel() {
+    public MusicViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is Music fragment");
     }
