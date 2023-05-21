8881dec57a508cbfd804beb655e82a1d412ad9f5
patrick Al Helou <Patrickalhelou@gmail.com>
2023-05-21 15:11:02 +0300

    history database workout done

--- app/src/main/java/com/example/mobileproject/WorkoutDetails.java
+++ app/src/main/java/com/example/mobileproject/WorkoutDetails.java
@@ -1,5 +1,6 @@
 package com.example.mobileproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -19,8 +20,12 @@ import android.widget.VideoView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
@@ -89,29 +94,61 @@ public class WorkoutDetails extends AppCompatActivity {
                 String title = getIntent().getStringExtra("title");
                 int sets = getIntent().getIntExtra("sets", 0);
 
-
                 // Update the sets count in Firebase Realtime Database
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("workouts").push();
+                DatabaseReference workoutsRef = database.getReference("workouts");
 
-                int setsTest = 0;
-                setsTest++;
-                myRef.child("name").setValue(name);
-                myRef.child("title").setValue(title);
-                myRef.child("sets").setValue(setsTest);
+                Query query = workoutsRef.orderByChild("name").equalTo(name);
 
-                // Update the progress bar
-                if(progressBar.getProgress() == 100)
-                {
-                    Toast.makeText(WorkoutDetails.this, "Congratulations, you have completed this workout", Toast.LENGTH_LONG).show();
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        boolean isWorkoutFound = false;
+                        if (dataSnapshot.exists()) {
+                            for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                                String workoutTitle = childSnapshot.child("title").getValue(String.class);
 
-                }
-                else
-                {
-                    progressBar.setProgress(progressBar.getProgress() + 100/sets);
-                }
+                                if (workoutTitle.equals(title)) {
+                                    // Increment the sets count
+                                    int setsTest = childSnapshot.child("sets").getValue(Integer.class);
+                                    setsTest++;
+                                    childSnapshot.getRef().child("sets").setValue(setsTest);
+                                    isWorkoutFound = true;
+                                    // Exit the listener
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (!isWorkoutFound) {
+                            // Create a new child with the incremented sets count
+                            DatabaseReference newWorkoutRef = workoutsRef.push();
+                            newWorkoutRef.child("name").setValue(name);
+                            newWorkoutRef.child("title").setValue(title);
+                            newWorkoutRef.child("sets").setValue(1);
+                        }
+
+                        query.removeEventListener(this); // Remove the listener
+
+                        // Update the progress bar
+                        if (progressBar.getProgress() == 100) {
+                            Toast.makeText(WorkoutDetails.this, "Congratulations, you have completed this workout", Toast.LENGTH_LONG).show();
+                        } else {
+                            progressBar.setProgress(progressBar.getProgress() + 100 / sets);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Handle the error
+                    }
+                });
             }
         });
+
+
+
+
     }
 
 
